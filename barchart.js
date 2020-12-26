bc1cbe7564c465e55666aa53010e20448489726b
Connie Ho <connie3@ualberta.ca>
2020-12-26 23:53:01 +0000

    appending bar elements

--- barchart.js
+++ barchart.js
@@ -2,29 +2,21 @@ $(document).ready(function(){
 
   function drawBarChart(data, options, element){
 
-    let figureContainer = $("<div></div>");
-    let graphContainer = $("<div></div>");
-    let barContainer = $("<div></div>");
+    let figureContainer = $("#figure")
+    let chartContainer = $("#chart")
 
-    let bars = [];
     let chartYMax = Math.max(...data)
 
     //Create a container for each bar
-    $.each(data, function(num){
-      for(let i = 0; i < data.length; i ++){
-        let barObj = {};
-        barObj.label = data[i];
-        barObj.height = Math.floor(data[i]/chartYMax * 100) + "%";
-        barObj.bar = $("<div><span>barObj.label</span></div>").appendTo(barContainer);
-        bars.push(barObj);
-      }
-    })
-
+    for(let num in data){
+      chartContainer.append("<div class=\"bar\">Hello</div>");
+    }
 
   }
 
-  data = [1,2,3,4,5]
+  data = [1,2,3,4,5];
   drawBarChart(data,null,"#chart");
+
 });
 
 
