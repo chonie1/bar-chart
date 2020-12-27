854a84fa4f005f8bbbc16c0b4fef9d99d844d8c8
Connie Ho <connie3@ualberta.ca>
2020-12-27 03:47:29 +0000

    bar chart drawn only with bars and labels thus far

--- barchart.js
+++ barchart.js
@@ -2,15 +2,27 @@ $(document).ready(function(){
 
   function drawBarChart(data, options, element){
 
-    let figureContainer = $("#figure")
-    let chartContainer = $("#chart")
+    let figureContainer = $("#figure");
+    let chartContainer = $("#chart");
+    let bars = []; //array for each bar
 
-    let chartYMax = Math.max(...data)
+    const chartYMax = Math.max(...data)*1.05; //Gives 5% margin from top
 
     //Create a container for each bar
-    for(let num in data){
-      chartContainer.append("<div class=\"bar\">Hello</div>");
+    for(let i = 0; i < data.length; i ++){
+      let barObj = {}
+      barObj.label = data[i];
+      barObj.height = Math.floor(data[i] / chartYMax * 100) + "%";
+      barObj.bar = $(`<div class=bar id=bar${i}><span id="label"${data[i]}>${barObj.label}</span></div>`)
+        .appendTo("#chart")
+        .height(barObj.height)
+        .width(1 / data.length * 100 + "%")
+      bars.push(barObj);
     }
+  }
+
+  function drawYAxis(min,max,increment){
+
 
   }
 
