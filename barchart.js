a95877a9a56fa2876ecf1b8599eb0e1b752f4200
Connie Ho <connie3@ualberta.ca>
2020-12-28 05:09:52 +0000

    added styling options

--- barchart.js
+++ barchart.js
@@ -3,6 +3,21 @@ $(document).ready(function(){
   //Hide all inputs first
   $("#titleForm").hide();
 
+  const data = [1,2,3,4,5];
+  const labelArr = ["label1","label2","label3","label4","label5"];
+  const options = {
+    barColor: "#ffb6c1",
+    labelColor: "white",
+    barSpacing: "10px solid white",
+    yScale: [1,5,2],
+    yLabelColor: "black",
+    yAxisColor: "black",
+    xLabelColor: "black",
+    titleSize: "30px",
+    titleColor: "#e75480",
+    valPos: "top"
+  };
+
   function nameXLabels(arr){
     for (let i = 0; i < arr.length; i ++) {
       $("#xLabel"+i).html(arr[i]);
@@ -19,16 +34,17 @@ $(document).ready(function(){
         .height(increment / (max - min + 1) * 100 + "%");
     }
   }
+
   function drawBarChart(data, options, element){
 
     let figureContainer = $("#figure");
     let chartContainer = $("#chart");
     let bars = []; //array for each bar
-    const chartYMax = Math.max(...data); //Gives 5% margin from top
-    const chartYMin = Math.min(...data);
+    const chartYMax = options["yScale"][1];
+    const chartYMin = options["yScale"][0];
 
     //Draws Y-axis with default min 0 and chartYMax incrementing by 1
-    drawYAxis(chartYMin,chartYMax,1);
+    drawYAxis(options["yScale"][0],options["yScale"][1],options["yScale"][2]);
 
     //Create a container for each bar
     for(let i = 0; i < data.length; i ++){
@@ -37,20 +53,52 @@ $(document).ready(function(){
       barObj.height = Math.floor(data[i] / chartYMax * 100) + "%";
       barObj.bar = $(`<div class=bar id=bar${i}>
       <div id="label"${data[i]} class="label">${barObj.label}</div>
-      <div id="xLabel${i}" class="xLabel">Hello</div>
+      <div id="xLabel${i}" class="xLabel">/div>
       </div>`)
         .appendTo("#chart")
         .height(barObj.height)
         .width(1 / data.length * 100 + "%")
       bars.push(barObj);
     }
+
+    //Name x-axis
+    nameXLabels(labelArr);
+
+    //Customize CSS based on options
+    for(let option in options){
+      if(option === "barColor"){
+        $(".bar").css("background-color",options[option]);
+      } else if(option === "labelColor"){
+        $(".label").css("color",options[option]);
+      } else if(option === "barSpacing"){
+        $(".bar").css("border-right",options[option]);
+        $(".bar").css("border-left",options[option]);
+      } else if(option ==="titleSize"){
+        $("#title").css("font-size",options[option]);
+      } else if(option ==="titleColor"){
+        $("#title").css("color",options[option]);
+      } else if(option ==="yLabelColor"){
+        $("#yAxisLabel").css("color",options[option]);
+      } else if(option ==="yAxisColor"){
+        $(".yBar").css("color",options[option]);
+      } else if(option === "xLabelColor"){
+        $(".xLabel").css("color",options[option]);
+      } else if(option === "valPos"){
+          if(options[option] === "top"){
+          } else if(options[option] === "middle"){
+            $(".label").css("bottom","50%");
+          } else if(options[option] === "bottom"){
+            $(".label").css("bottom","0%");
+          }
+
+      }
+    }
+
   }
 
-  const data = [1,2,3,4,5];
-  const labelArr = ["label1","label2","label3","label4","label5"];
 
-  drawBarChart(data,null,"#chart");
-  nameXLabels(labelArr);
+  drawBarChart(data,options,"#figure");
+
 
 
 
@@ -71,6 +119,7 @@ $(document).ready(function(){
         $("#title").show();
       }
     });
+
 });
 
 
